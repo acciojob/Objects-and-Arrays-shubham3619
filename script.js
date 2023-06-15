cc281526dbd9f24a038130a1a8cbc754f0ea2b1d
shubham3619 <120643298+shubham3619@users.noreply.github.com>
2023-06-15 10:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+var team = players; // Creating a reference to the players array
+
+var team1 = [...players]; // Creating a copy of the players array using the spread operator
+
+var cap1 = { ...person }; // Creating a copy of the person object using the spread operator
+
+// Testing the variables
+console.log(team);   // Output: ["John", "Mike", "Sarah"]
+console.log(team1);  // Output: ["John", "Mike", "Sarah"]
+console.log(cap1);   // Output: { name: "Alice", age: 25, role: "Captain" }
 
 window.players = players;
 window.person = person;
